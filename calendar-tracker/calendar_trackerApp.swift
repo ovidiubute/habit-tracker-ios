b5cfa0aa523cd23bb526f2a9bcf5e3676c01b0b8
Ovidiu Bute <ovidiu.bute@gmail.com>
2025-09-20 08:49:26 +0300

    Initial Commit

---
+++ calendar-tracker/calendar_trackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  calendar_trackerApp.swift
+//  calendar-tracker
+//
+//  Created by Ovidiu on 20.09.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct calendar_trackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
